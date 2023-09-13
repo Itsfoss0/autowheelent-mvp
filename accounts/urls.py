de4b8f56586b639d5268dffbe7ee130208b75b4f
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-13 21:59:20 +0300

    [Chore]: Add Accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Urls related to user accounts
+"""
+
+from django.urls import path
+from accounts.views import account
+
+
+urlpatterns = [
+    path('', account, name="account")
+]
