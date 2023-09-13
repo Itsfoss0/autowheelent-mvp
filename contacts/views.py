aa20a67f4b03c9e6099b31272b500b8385a12643
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-13 22:01:35 +0300

    [Chore]: Add contacts app

---
+++ contacts/views.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Views related to contacts
+"""
+
+from django.http import HttpResponse
+
+def contacts(request):
+    """
+    Contacts page
+    To be implemented
+    """
+    return HttpResponse('Contacts .... coming soon')
