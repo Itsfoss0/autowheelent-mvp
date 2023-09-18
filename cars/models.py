5b5e2573f948783dc17d4ebd612e181d63ec6c78
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-18 15:53:31 +0300

    [Chore]: Add Car models - v1

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    """
+    Car model
+    """
+    id = models.IntegerField(primary_key=True)
+    color = models.TextField(max_length=30)
+    car_title = models.TextField(max_length=30, null=False)
+    city = models.TextField(max_length=30, null=False)
+    model = models.TextField(max_length=30)
+    year = models.DateField(auto_created=True, unique=False)
+    body_style = models.TextField(max_length=30)
+    fuel_type = models.TextField(max_length=30)
+    is_featured = models.TextField(max_length=30)
+    
+
+    def create(self):
+        self.save()
