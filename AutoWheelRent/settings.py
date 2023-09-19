c73a16d38e72caf5b7676db033b303b4bdcba873
Mercy Kalondu <mercykalondu62@gmail.com>
2023-09-19 12:32:30 +0000

    settings update

--- AutoWheelRent/settings.py
+++ AutoWheelRent/settings.py
@@ -150,3 +150,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email sending
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'autowheelrent@gmail.com'
+EMAIL_HOST_PASSWORD = 'autowheetrent123'
+EMAIL_USE_TLS = True
