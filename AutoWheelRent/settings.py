57341f7ae679c416f05dd072d2c4e0735889f87c
Ebenezer <akhonyaebenezer@gmail.com>
2023-10-05 03:22:38 +0300

    [Chore]: Update static media

--- AutoWheelRent/settings.py
+++ AutoWheelRent/settings.py
@@ -147,6 +147,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'AutoWheelRent/static'),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
